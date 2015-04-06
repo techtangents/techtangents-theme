bd4c5e494e6f34b67b275b3102c794bbd8d67d73
techtangents <dylan@techtangents.com>
2015-04-06 20:37:07 +1000

    header

--- theme/header.php
+++ theme/header.php
@@ -22,12 +22,23 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'techtangents' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
+		<div class="site-title">
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+				<img
+					src="<?php echo get_template_directory_uri() ?>/images/techtangents.gif"
+					alt='techtangents'
+					title='techtangents'
+				/>
+			</a>
+		</div>
+
 		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation" role="navigation">
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">&sigma;</a>
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php _e( 'Primary Menu', 'techtangents' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
